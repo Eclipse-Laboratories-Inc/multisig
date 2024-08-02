be4404b1be9baeb126899b9839bc82eccd76becd
Yuri Albuquerque <yuridenommus@gmail.com>
2024-08-02 11:25:29 -0300

    Use the nixfmt officially used by nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,23 +3,35 @@
     nixpkgs.url = "github:Denommus/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, flake-utils, nixpkgs, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in {
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
         devShells.default = pkgs.mkShell {
           pname = "multisig-shell";
 
-          buildInputs = [
-            pkgs.anchor
-            pkgs.rustup
-            pkgs.pkg-config
-            pkgs.nodejs_latest
-            pkgs.nixfmt
-          ] ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
-            pkgs.iconv
-            pkgs.libiconv
-          ]);
+          buildInputs =
+            [
+              pkgs.anchor
+              pkgs.rustup
+              pkgs.pkg-config
+              pkgs.nodejs_latest
+              pkgs.nixfmt-rfc-style
+            ]
+            ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
+              pkgs.iconv
+              pkgs.libiconv
+            ]);
         };
-      });
+      }
+    );
 }
