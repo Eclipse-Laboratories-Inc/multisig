2c02c640a2676050e03b188023f92de5e568236f
Yuri Albuquerque <yuridenommus@gmail.com>
2024-08-02 10:46:25 -0300

    Install and use nixfmt

--- flake.nix
+++ flake.nix
@@ -4,24 +4,22 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, flake-utils, nixpkgs, ... }:
-  flake-utils.lib.eachDefaultSystem (system:
-  let
-    pkgs = import nixpkgs {
-      inherit system;
-    };
-  in {
-    devShells.default = pkgs.mkShell {
-      pname = "multisig-shell";
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          pname = "multisig-shell";
 
-      buildInputs = [
-        pkgs.anchor
-        pkgs.rustup
-        pkgs.pkg-config
-        pkgs.nodejs_latest
-      ] ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
-        pkgs.iconv
-        pkgs.libiconv
-      ]);
-    };
-  });
+          buildInputs = [
+            pkgs.anchor
+            pkgs.rustup
+            pkgs.pkg-config
+            pkgs.nodejs_latest
+            pkgs.nixfmt
+          ] ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.iconv
+            pkgs.libiconv
+          ]);
+        };
+      });
 }
